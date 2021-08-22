871ff1024d2921104136ed3d3eeb45e81d6c84d7
Miki-8y <taichan.y.n.n@gmail.com>
2021-08-22 16:44:55 +0900

    アイテムの処理ファイル

---
+++ EscapeGames-item_v2/Assets/Scripts/Enum.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ユーザ操作によって状態が変わるもの
+public enum eventList{
+
+    LIST_LEN
+}
+public enum itemList{
+    ink, 
+    hasami, 
+    LIST_LEN
+}
+
+//Scene名と同じ
+public enum SceneList{
+
+    LIST_LEN
+}
---
+++ EscapeGames-item_v2/Assets/Scripts/ItemListController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemListController : MonoBehaviour
+{
+    public GameObject[] ItemSlots;
+    
+    private int now = 0;
+
+    public GameObject selected;
+
+    public void Move(string direction) {
+        ItemSlots[now].SetActive(false);
+        
+        if(direction == "right"){
+            now += 1;
+        }else{
+            now -= 1;
+        }
+
+        if(now > 3){
+            now = 0;
+        }else if(now < 0){
+            now = 3;
+        }
+
+        ItemSlots[now].SetActive(true);
+
+        GameObject[] selects = GameObject.FindGameObjectsWithTag("Selected");
+            //アイテムをクリックするのに連動して赤い枠の表示切替
+            //アイテムスロット(青いボタン)をクリックすると全てのアイテムの選択を解除するようにしている
+            foreach(GameObject select in selects){
+                select.SetActive(false);
+                }
+    }
+
+}
